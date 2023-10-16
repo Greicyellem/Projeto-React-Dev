ac2de0d31324dd118dc35152e7c46929bb768352
greicyellem <greicysantana1@gmail.com>
2023-10-16 15:05:14 -0300

    Projeto de Desenvolvimento Web com React - V. Prof

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import { config } from "dotenv";
+
+config(); // Carrega as variáveis de ambiente do arquivo .env
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+});
---
+++ pages/products/index.js
@@ -0,0 +1,62 @@
+'use client'
+import 'tailwindcss/tailwind.css'
+import Appbar from '@/app/components/Appbar';
+import Bottom from '@/app/components/Bottom';
+import Drawer from '@/app/components/Drawer';
+import React, {useContext, useEffect, useState} from 'react';
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+import { fetchProducts } from '@/app/utils/api'
+import { CartContext } from '@/app/contexts/CartContext';
+import { ProductContainer, ProductImage, CardButton } from '@/app/styles/ProductsStyles'
+
+const ProductsPage = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState();
+  const {addToCart} = useContext(CartContext);
+
+  const handleMenuToggle  = () => {
+    setIsDrawerOpen(!isDrawerOpen)
+  }
+
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+        const productsData = await fetchProducts();
+        setProducts(productsData);
+    }
+    getProducts();
+  }, []);
+
+  
+  return (
+    <main className="min-h-screen">
+      <Appbar onMenuToggle={handleMenuToggle}></Appbar>
+      <Drawer isOpen={isDrawerOpen} onClose={handleMenuToggle}></Drawer>
+      <ul>
+        {products.map((product) =>(
+            <li key={product.id}>
+              <ProductContainer>
+                <ProductImage src={product.image} width={200} />
+                <p>{product.title}</p>
+                <p>{product.price}</p> 
+                <p>{product.description}</p>
+                <p>{product.category} </p>
+                <CardButton 
+                  onClick={() => addToCart(product)}
+                >
+                  Add cart
+                </CardButton>
+              </ProductContainer>
+            </li>
+        ))}
+      </ul>
+      <Bottom></Bottom>
+    </main>
+  );
+
+}
+export default ProductsPage;
